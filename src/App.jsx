b4e9d990fe930bbbdba9827afa88ce4c90285b17
Jubayer Shikder <validjubayer@gmail.com>
2025-06-07 16:44:13 +0600

    Add DarkMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { RouterProvider } from "react-router";
 import { router } from "./routes/Routes";
 import AuthProvider from "./context/authContext/AuthProvider";
+import * as Tooltip from "@radix-ui/react-tooltip";
 
-const App = () => {
-         return (
-          <AuthProvider>
-            <RouterProvider router={router} />
-          </AuthProvider>
-        );
-};
+function App() {
+  return (
+      <Tooltip.Provider>
+        <AuthProvider>
+          <RouterProvider router={router} />
+        </AuthProvider>
+      </Tooltip.Provider>
+  );
+}
 
 export default App;

24d7c3646b36dcae718da94b5c19e7873039d8a1
Jubayer Shikder <validjubayer@gmail.com>
2025-06-18 14:57:59 +0600

    Fix Problem on FoodDetails; Fix Minors

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -33,22 +33,28 @@ const FoodDetails = () => {
       </p>
       <div className=" border rounded-xl p-5 dark:text-white">
         <img src={img} alt={""} className="rounded-xl w-full " />
-        <h3 className="text-4xl font-bold text-center dark:text-white">{food_name}</h3>
+        <h3 className="text-4xl font-bold text-center dark:text-white">
+          {food_name}
+        </h3>
       </div>
       {/* box starts */}
       <p className="text-2xl font-semibold text-center py-2 dark:text-white">
         Available Stock: {available_quantity} {food_name}
       </p>
       <form className="w-full my-2">
-        {author_email !== user?.email && <input
-          className="input w-1/2"
-          placeholder="Quantity"
-          max={available_quantity}
-          type="number"
-          required
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        />}
+        {author_email !== user?.email ? (
+          <input
+            className="input w-1/2"
+            placeholder="Quantity"
+            max={available_quantity}
+            type="number"
+            required
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+        ) : (
+          ""
+        )}
 
         <Link
           to={{
@@ -63,9 +69,11 @@ const FoodDetails = () => {
         >
           <button
             disabled={isDisabled}
-            className="btn disabled:bg-gray-400 disabled:cursor-not-allowed w-1/2"
+            className={`btn disabled:bg-gray-400 disabled:cursor-not-allowed ${
+              user && author_email === user?.email ? "w-full" : "w-1/2"
+            } `}
           >
-            {author_email === user?.email
+            {user && author_email === user?.email
               ? "Update"
               : available_quantity === 0
               ? "item is not available"
